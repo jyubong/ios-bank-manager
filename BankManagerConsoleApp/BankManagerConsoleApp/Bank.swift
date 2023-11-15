d0278aea6c0eebf73d9b2d44317b26d6ad104803
woohyeon3382 <woohyeon3382@naver.com>
2023-11-15 17:48:59 +0900

    feat: Bank 타입 구현 및 BankManager 생성

---
+++ BankManagerConsoleApp/BankManagerConsoleApp/Bank.swift
@@ -0,0 +1,13 @@
+//
+//  Bank.swift
+//  BankManagerConsoleApp
+//
+//  Created by jyubong, Toy on 11/15/23.
+//
+
+import Foundation
+import BankManager
+
+struct Bank {
+    private let bankManager = BankManager()
+}
